06f987029945a96610aec4820102c6a16f002157
Josh Machado <joshomac505@gmail.com>
2024-10-19 17:31:47 -0700

    additional stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-echo BACKEND_DOMAIN
-echo BACKEND_HOST
-echo BACKEND_PORT
-echo FRONTEND_DOMAIN
-echo FRONTEND_HOST
-echo FRONTEND_PORT
+echo ${BACKEND_DOMAIN}
+echo ${BACKEND_HOST}
+echo ${BACKEND_PORT}
+echo ${FRONTEND_DOMAIN}
+echo ${FRONTEND_HOST}
+echo ${FRONTEND_PORT}
+echo
 
 # for backwards compatibility, seperates host and port from url
 export FRONTEND_DOMAIN=${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
@@ -15,6 +16,11 @@ export FRONTEND_PORT=${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
 
 export BACKEND_DOMAIN=${BACKEND_DOMAIN:-${BACKEND_HOST%:*}}
 export BACKEND_PORT=${BACKEND_PORT:-${BACKEND_HOST##*:}}
+
+echo ${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
+echo ${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
+echo ${BACKEND_DOMAIN:-${BACKEND_HOST%:*}}
+echo ${BACKEND_PORT:-${BACKEND_HOST##*:}}
 
 # strip https:// or https:// from domain if necessary
 FRONTEND_DOMAIN=${FRONTEND_DOMAIN##*://}
